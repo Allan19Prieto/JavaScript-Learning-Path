3fe0ef160fc5eb91e0dab6049ec23ab55d38c9b1
Allan Prieto <allanbanilla@gmail.com>
2023-07-31 18:51:34 -0600

    Clase variables

---
+++ Fundamentos-JavaScript/tarea.js
@@ -0,0 +1,16 @@
+// tarea 1
+
+let admin, name;
+
+name = 'Jhon'
+
+admin = name;
+
+alert(admin)
+
+// tarea 2
+let ourPlanetName = 'Tierra'; 
+let currentUserName = 'Allan';
+
+const BIRTHDAY = '16.04.1999'
+const age = someCode(birthday)
