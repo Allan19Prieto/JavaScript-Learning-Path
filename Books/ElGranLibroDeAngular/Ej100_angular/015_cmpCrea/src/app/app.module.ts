8fd68962b29d58faa74b5b2ea63918ef3de9922c
Allan19Prieto <allanbanilla@gmail.com>
2025-09-30 18:52:49 -0600

    Ejemplos

---
+++ Books/ElGranLibroDeAngular/Ej100_angular/015_cmpCrea/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration, withEventReplay } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { Compo1Component } from './compo1/compo1.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    Compo1Component
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [
+    provideClientHydration(withEventReplay())
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ Books/ElGranLibroDeAngular/Ej100_angular/015_cmpCrea/src/app/compo1/compo1.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-compo1',
+  standalone: false,
+  templateUrl: './compo1.component.html',
+  styleUrl: './compo1.component.css'
+})
+export class Compo1Component {
+
+}
